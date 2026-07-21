f5d9ba705486f6967f8609e6bbcdbc4994eb5193
JonathanCilley <jonathan.cilley@gmail.com>
2026-07-20 20:32:00 -0400

    feat: apply GPLv3 open source license and file headers

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,6 @@
+// Copyright (C) 2026 Jonathan Cilley <jonathan.cilley@gmail.com>
+// SPDX-License-Identifier: GPL-3.0-only
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
